b508e3fdf4b874c337e90fb5cdf6249ca7d9d488
Nischay Mamidi <NischayPro@gmail.com>
2017-09-30 14:48:44 +0530

    script: add notify event

--- content_scripts/manga.js
+++ content_scripts/manga.js
@@ -1,1 +1,10 @@
 document.body.style.border = "5px solid red";
+
+notify("Test","djsd","basic");
+function notify(title, content, type) {
+    browser.runtime.sendMessage({
+        title: title,
+        content: content,
+        type: type
+    });
+}
