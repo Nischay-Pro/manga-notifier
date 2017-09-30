60f243781db89b06cf1a83bd125c4ffe2dcba59d
C Shri Akhil <shriakhilc@gmail.com>
2017-09-30 20:17:55 +0530

    Add options/ and change parsing tags
    Now targets the tags to get series from the series specific page rather
    than the common releases page

--- content_scripts/manga.js
+++ content_scripts/manga.js
@@ -1,11 +1,31 @@
 console.log("Script Start");
-document.body.style.border = "5px solid red";
-var titles = document.getElementsByClassName("title");
+
+// START - Border Color Block
+
+function onError(error) {
+  console.log(`Error: ${error}`);
+}
+
+function onGot(item) {
+  var color = ["red","blue"];
+  if (item.color) {
+    color = item.color;
+  }
+  console.log(color);
+  document.body.style.border = "10px solid " + color[1];
+}
+
+var getting = browser.storage.local.get("color");
+getting.then(onGot, onError);
+
+// END - Border Color Block
+
+var titles = document.getElementsByClassName("tips"); // "tips" is for the series specific page parsing
 console.log(titles.length);
 [].forEach.call(titles, function (title) {
-  // console.log(title.innerText);
+  console.log(title.innerText);
   // The "new", "hot" and such tags are being added in innerText by mangafox. Need to trim.
-    title.getElementsByTagName('a')[0].style.color = "green"; // marking as done
+    title.style.color = "green"; // marking as done
 })
 
 notify("Test","djsd","basic");
