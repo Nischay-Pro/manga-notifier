d66f18bc4d7483a59ed857ad82eedfc425ad9300
C Shri Akhil <shriakhilc@gmail.com>
2017-09-30 15:19:40 +0530

    Add title parsing
    Prints total count to log

--- content_scripts/manga.js
+++ content_scripts/manga.js
@@ -1,1 +1,9 @@
+console.log("Script Start");
 document.body.style.border = "5px solid red";
+var titles = document.getElementsByClassName("title");
+console.log(titles.length);
+[].forEach.call(titles, function (title) {
+  // console.log(title.innerText);
+  // The "new", "hot" and such tags are being added in innerText by mangafox. Need to trim.
+    title.getElementsByTagName('a')[0].style.color = "green"; // marking as done
+})
