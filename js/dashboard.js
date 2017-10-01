3b2624817a5d573ff11da979fd7986e1abd8db32
Nischay Mamidi <NischayPro@gmail.com>
2017-10-01 07:29:37 +0530

    options: added tags

--- js/dashboard.js
+++ js/dashboard.js
@@ -1,26 +1,37 @@
 function saveOptions(e) {
   e.preventDefault();
   browser.storage.local.set({
-    frequency: $("#interval").children(":selected").attr("value")
+    data: {
+      frequency: $("#interval").children(":selected").attr("value"),
+      mangaTags: $('#chips').material_chip('data')
+    }
   });
   document.getElementById('frequencynot').innerText = "Current Frequency for checking: " + $("#interval").children(":selected").attr("value") + " hour(s)";
+  Materialize.toast('Changes saved successfully!', 4000)
 }
 
 function restoreOptions() {
 
   function setCurrentChoice(result) {
-    console.log(result.frequency);
-    document.getElementById('interval').value = result.frequency || 1;
-    $("#interval").val(result.frequency || 1).trigger('change');
-    document.getElementById('frequencynot').innerText = "Current Frequency for checking: " + (result.frequency || 1) + " hour(s)";
-
+    console.log(result.data.frequency);
+    document.getElementById('interval').value = result.data.frequency || 1;
+    $("#interval").val(result.data.frequency || 1).trigger('change');
+    document.getElementById('frequencynot').innerText = "Current Frequency for checking: " + (result.data.frequency || 1) + " hour(s)";
+    $('#chips').material_chip({
+      placeholder: 'Enter a tag',
+      limit: Infinity,
+      minLength: 1,
+      data: result.data.mangaTags
+    });
+    console.log(result.data.mangaTags);
   }
 
   function onError(error) {
     console.log(`Error: ${error}`);
+    Materialize.toast(`Error: ${error}`, 4000)
   }
 
-  var getting = browser.storage.local.get("frequency");
+  var getting = browser.storage.local.get("data");
   getting.then(setCurrentChoice, onError);
 }
 
@@ -37,10 +48,15 @@ function formatState(state) {
 $('.js-example-basic-single').select2({
   templateSelection: formatState
 });
+$('.chips').material_chip({
+  placeholder: 'Enter a tag',
+  limit: Infinity,
+  minLength: 1
+});
 document.addEventListener("DOMContentLoaded", restoreOptions);
 document.querySelector("form").addEventListener("submit", saveOptions);
-(function($){
-  $(function(){
+(function ($) {
+  $(function () {
 
     $('.button-collapse').sideNav();
 
