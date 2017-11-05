5f7d3193bdf0f87a0595937e250818ad50129efc
C Shri Akhil <shriakhilc@gmail.com>
2017-11-05 19:48:25 +0530

    Add MangaStream support
    Now supports addition of URLs from (Mangastream)[https://mangastream.com/]
    
    Fixes #4

--- content_scripts/background.js
+++ content_scripts/background.js
@@ -1,6 +1,7 @@
 var nIntervId = 0;
 var currFreq = 1;
 var running = 0;
+var mangastream_base_url = "https://mangastream.com/";
 
 function notify(message) {
     var data = message;
@@ -33,6 +34,7 @@ function refresh(){
   getting.then(function(res){
     // console.log(res);
     // Check if freq is the same. Else clear and restart.
+
     if(res.data.frequency == currFreq){
       var links = res.data.mangaTags;
       getContent(links,0);
@@ -58,38 +60,62 @@ function getContent(links,ind) {
       // console.log("text parsing begins. status " + this.status);
       var parser = new DOMParser();
       var doc = parser.parseFromString(this.responseText, "text/html");
-      var titles = doc.getElementsByClassName("tips");
-      if(titles[0] !== void 0){
-        // do some notif checks here
+
+      var latestTitle = "";
+      if(/mangafox/i.test(url)){
+        // Mangafox link. Parse accordingly
+        var titles = doc.getElementsByClassName("tips");
+        latestTitle = titles[0];
+        // console.log(latestTitle.innerText);
+      } else if (/mangastream/i.test(url)) {
+        // Mangastream url. Parse accordingly
+        var rows = doc.getElementsByTagName('tr'); // Collection of rows
+        var r0 = rows[1]; // Start with 1 because row[0] is the header of the table i.e. 'Chapter' and 'Released'
+        // console.log(r0);
+        var child0 = r0.children[0]; // First <td> in the <tr>, i.e. Name of Chapter
+        // console.log(child0);
+        // console.log("Chapter Name: " + child0.innerText); // Chapter Name
+        latestTitle = child0;
+
+        var link0 = child0.firstChild; // <a href>
+        // console.log(link0);
+        // console.log("Path: " + link0.pathname);
+        // console.log("Link: " + mangastream_base_url + link0.pathname); // URL to chapter
+      }
+
+      if(latestTitle !== void 0){
+        // chapters exist. test if notif needed
         var lastSeen = browser.storage.local.get(url);
         lastSeen.then(function (res) {
           if(res[url] !== void 0){
             // value exists
-            if(res[url] !== titles[0].innerText){
+            if(res[url] !== latestTitle.innerText){
               //something new. Notify user.
               //innerText could be trimmed to remove chapter number
               notify({
                 type: "basic",
-                title: titles[0].innerText,
+                title: latestTitle.innerText,
                 content: "New content uploaded."
               });
               // modify last seen chapter
               var obj = {};
-              obj[url] = titles[0].innerText;
+              obj[url] = latestTitle.innerText;
               browser.storage.local.set(obj);
             }
             // else {
+            //   // No new chapters case. Can be used to test features while debugging. Should be commented out in release versions.
             //   console.log("No new chapters for " + res[url]);
+            //   // to test the notifs
             //   notify({
             //     type: "basic",
-            //     title: titles[0].innerText,
+            //     title: latestTitle.innerText,
             //     content: "No new content."
             //   });
             // }
           } else {
             // add value
             var obj = {};
-            obj[url] = titles[0].innerText;
+            obj[url] = latestTitle.innerText;
             browser.storage.local.set(obj);
           }
         });
